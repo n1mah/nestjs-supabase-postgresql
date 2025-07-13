9b686d0fdec44639871b1885a59a9fe39cf2e892
Nima Heidari <nima.heidari.n@gmail.com>
2025-07-13 22:50:46 +0330

    Feat: Add createProduct (Post Method)

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ProductService } from './product.service';
+import { Product } from './product.entity';
 
 @Controller('product')
-export class ProductController {}
+export class ProductController {
+  constructor(private readonly productService: ProductService) {}
+
+  @Post()
+  async createProduct(@Body() body: Partial<Product>): Promise<Product> {
+    return this.productService.create(body);
+  }
+}

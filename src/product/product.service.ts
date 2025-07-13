26a8a78e6a66f45c80dddd6dbefdfa5019db5407
Nima Heidari <nima.heidari.n@gmail.com>
2025-07-13 22:47:56 +0330

    Feat: Implement Create Method & Inject Repository in ProductService

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Product } from './product.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+  ) {}
+
+  async create(productData: Partial<Product>): Promise<Product> {
+    const product = this.productRepository.create(productData);
+    return this.productRepository.save(product);
+  }
+}

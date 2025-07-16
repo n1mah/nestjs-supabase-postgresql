cd4ac87543cce57579db0e2feea2daa8e387a6ed
Nima Heidari <nima.heidari.n@gmail.com>
2025-07-16 16:47:31 +0330

    Fix: Add form-data for send Image & Pass To Service for Create

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,13 +1,24 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { ProductService } from './product.service';
 import { Product } from './product.entity';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('product')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
   @Post()
-  async createProduct(@Body() body: Partial<Product>): Promise<Product> {
-    return this.productService.create(body);
+  @UseInterceptors(FileInterceptor('image'))
+  async createProduct(
+    @UploadedFile() file: Express.Multer.File,
+    @Body() body: { title: string; price: number },
+  ): Promise<Product> {
+    return this.productService.create(body, file);
   }
 }
